337736d15fe68ce2c3a2a71baca5a3df93d74df6
Juan Camilo Lopez J. <jucalo2195@gmail.com>
2025-02-18 14:58:01 -0500

    Remove mermaid.py

--- agent/mermaid.py
+++
@@ -1,5 +0,0 @@
-from IPython.display import Image, display
-from langchain_core.runnables.graph import MermaidDrawMethod
-from graph import graph_builder
-
-display(Image(graph_builder.get_graph(xray=2).draw_mermaid_png()))
